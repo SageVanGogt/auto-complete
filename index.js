7dada30bd1eea27da6729f788bba6aab83efc961
Sage Vogt <thurmanvogt@gmail.com>
2018-04-11 14:27:45 -0600

    Update to support weighted suggest

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ class Node {
     this.letter = letter;
     this.children = {};
     this.isWord = false;
+    this.chosen = 0;
   }
 }
 
 class Trie {
   constructor() {
-    this.root = new Node(null)
+    this.root = new Node(null);
     this.wordCount = 0; 
-    this.suggestions = [];   
+    this.suggestions = [];  
+    this.sortedSuggestions = [];
   }
 
-  add(word) {
+  add(inputWord) {
+    let word = inputWord.toLowerCase();
     let currentNode = this.root;
 
     for (let i = 0; i < word.length; i++) {
       if (!currentNode.children[word[i]]) {
         currentNode.children[word[i]] = new Node(word[i]);
-      } 
-
+      }
       currentNode = currentNode.children[word[i]];
     }
     if (!currentNode.isWord) {
@@ -39,44 +41,77 @@ class Trie {
       if(!child) {
         return null;
       }
-  
       currentNode = child; 
     }
     return currentNode
   }
-
-  suggest(str) {
-    this.suggestions = [];
+  
+  suggest(inputStr) {
+    this.resetSuggestions();
+    let str = inputStr.toLowerCase();
     let currentNode = this.findStartNode(str);
     
     if(!currentNode) { return null };
+    this.findWordSuggestions(currentNode, str); //create suggestions array w unsorted objects
 
-    this.findWordSuggestions(currentNode, str);
-
-    return this.suggestions;
+    this.sortSuggestions(); //takes suggestions array full of objects and sorts them by weight
+    this.getSuggestions(); //iterates over sorted suggestions and pushed obj.word into sortedsuggestions array
+  
+    return this.sortedSuggestions;
   }
   
   findWordSuggestions(startingNode, prefix) {
     if (startingNode.isWord) {
-      this.suggestions.push(prefix);
+      this.formatSuggestion(startingNode, prefix)
     }
     Object.keys(startingNode.children).forEach( childLetter => {
       let currentNode = startingNode.children[childLetter];
-      
       return this.findWordSuggestions(currentNode, prefix + childLetter);      
     })
   }
+  
+  sortSuggestions() {
+    this.suggestions.sort( (a, b) => {
+      return b.commonality - a.commonality;
+    })
+  }
 
+  getSuggestions() {
+    this.suggestions.forEach(wordObj => {
+      this.sortedSuggestions.push(wordObj.word);
+    })
+  }
+
+  formatSuggestion(node, word) {
+    this.suggestions.push({
+      word: word,
+      commonality: node.chosen
+    });
+  }
+  
   delete(str) {
-    let currentNode = this.findStartNode(str)
+    let currentNode = this.findStartNode(str);
+    
     if (currentNode.isWord) {
       currentNode.isWord = false;
       this.wordCount--;
     }
   }
-  
-  populate(data) {
-    data.forEach( word => this.add(word));
+
+  select(word) {
+    let currNode = this.findStartNode(word);
+
+    currNode.chosen++;
   }
 
+  resetSuggestions() {
+    this.suggestions = [];
+    this.sortedSuggestions = [];
+    
+  }
+  
+  populate(dictionary) {
+    dictionary.forEach( word => this.add(word));
+  }
+  
 }
